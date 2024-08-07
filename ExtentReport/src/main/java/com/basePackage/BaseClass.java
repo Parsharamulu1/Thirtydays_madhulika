479a730a46f8596e440060da475d08c8a64a4e24
parsharamulu.arukuta <parsharamulu.arukuta@GGKU5DELL1629.acs.net>
2024-08-07 12:10:37 +0530

    extent report changes

---
+++ ExtentReport/src/main/java/com/basePackage/BaseClass.java
@@ -0,0 +1,16 @@
+package com.basePackage;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
+
+public class BaseClass {
+	
+	public ExtentSparkReporter extentsparkreporter;
+	public ExtentReports extent;
+	public ExtentTest test;
+	
+
+}
